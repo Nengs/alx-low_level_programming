c90bf4b04d9d4c49d343af06be399be855c2b0f7
Nengs <Ninetysnengs@gmail.com>
2023-03-22 16:35:24 -0700

    C - Functions, nested loops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,42 +1,40 @@
-#include "main.h"
-
 /**
  * times_table - prints the 9 times table starting from 0
  *
- * Return: void
+ * Return: Nothing
  */
 void times_table(void)
 {
-    int i, j, n;
+        int i, j, n;
 
-    for (i = 0; i <= 9; i++)
-    {
-        for (j = 0; j <= 9; j++)
+        for (i = 0; i <= 9; i++)
         {
-            n = i * j;
+                for (j = 0; j <= 9; j++)
+                {
+                        n = i * j;
 
-            if ((n / 10) == 0)
-            {
-                if (j != 0)
-                    _putchar(' ');
-                _putchar(n + '0');
+                        if ((n / 10) == 0)
+                        {
+                                if (j != 0)
+                                        _putchar(' ');
+                                _putchar(n + '0');
 
-                if (j == 9)
-                    continue;
-                _putchar(',');
-                _putchar(' ');
-            }
-            else
-            {
-                _putchar((n / 10) + '0');
-                _putchar((n % 10) + '0');
-                if (j == 9)
-                    continue;
-                _putchar(',');
-                _putchar(' ');
-            }
+                                if (j == 9)
+                                        continue;
+                                _putchar(',');
+                                _putchar(' ');
+                        }
+                        else
+                        {
+                                _putchar((n / 10) + '0');
+                                _putchar((n % 10) + '0');
+                                if (j == 9)
+                                        continue;
+                                _putchar(',');
+                                _putchar(' ');
+                        }
+                }
+                _putchar('\n');
         }
-        _putchar('\n');
-    }
 }
 
