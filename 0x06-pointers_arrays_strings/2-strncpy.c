4dda56fb1121396109e980aa0fbcd5c66e044080
Nengs <Ninetysnengs@gmail.com>
2023-03-30 11:56:18 -0700

    C - More pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+* _strncpy - copy a string
+* @dest: destination string
+* @src: source string
+* @n: number of bytes to be copied
+*
+* Return: pointer to the resulting string
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+int i = 0;
+
+while (src[i] && i < n)
+{
+dest[i] = src[i];
+i++;
+}
+while (i < n)
+{
+dest[i] = '\0';
+i++;
+}
+
+return (dest);
+}
+
